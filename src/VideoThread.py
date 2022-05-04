5e9775c5636691053bd75439f3a08e03bc55f4a2
Furkan Demir <furkan_demir00@hotmail.com>
2022-05-04 03:45:45 +0300

    analysis for multithread
    analysis for multithread implemented

--- src/VideoThread.py
+++ src/VideoThread.py
@@ -169,6 +169,7 @@ class VideoSingleThread(QThread):
 class VideoThread(QThread):
     ImageUpdate = pyqtSignal(QImage) #thread signal forward attachment
     ValChanged = pyqtSignal(int) #camera check forward
+    Analysis_Thread = pyqtSignal(list) #list of analysis of emotions
     
     def __init__(self, threadID, name):
         super().__init__()
@@ -181,7 +182,17 @@ class VideoThread(QThread):
         self.configFile = "deploy.prototxt.txt" 
         self.labelColor = (10, 10, 255)
         self.chosen = 0
-    
+        self.average_emotions = np.zeros(7, dtype=int)
+#        self.average_emotions[0] = 0 #Angry
+#        self.average_emotions[1] = 0 #Disgust
+#        self.average_emotions[2] = 0 #Fear
+#        self.average_emotions[3] = 0 #Happy
+#        self.average_emotions[4] = 0 #Sad
+#        self.average_emotions[5] = 0 #Surprised
+#        self.average_emotions[6] = 0 #Neutral
+        self.captured_emotions = self.average_emotions.copy()
+
+        
     def run(self):
         self.ThreadActive = True
         cap = cv2.VideoCapture(self.threadID)
@@ -192,7 +203,7 @@ class VideoThread(QThread):
         self.replayVid = False
         self.openVid = False
         self.videoPath = None
-        
+        maxed_emotion = 'Neutral'
         while self.ThreadActive:
             if self.replayVid and self.videoPath != None:
                 cap = cv2.VideoCapture(self.videoPath)
@@ -213,6 +224,7 @@ class VideoThread(QThread):
                 net.setInput(blob)
                 faces = net.forward()
                 #faces = self.face_haar_cascade.detectMultiScale(gray_image)
+                self.captured_emotions = np.zeros(7, dtype=int)
                 try:
                     for i in range(0, faces.shape[2]):
                         if self.pauseVid:
@@ -233,14 +245,55 @@ class VideoThread(QThread):
                             max_index = np.argmax(predictions[0])
                             emotion_detection = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprised', 'Neutral')
                             emotion_prediction = emotion_detection[max_index]
+                            if emotion_prediction == 'Angry':
+                                self.captured_emotions[0] +=1
+                            elif emotion_prediction == 'Disgust':
+                                self.captured_emotions[1] +=1
+                            elif emotion_prediction == 'Fear':
+                                self.captured_emotions[2] +=1
+                            elif emotion_prediction == 'Happy':
+                                self.captured_emotions[3] +=1
+                            elif emotion_prediction == 'Sad':
+                                self.captured_emotions[4] +=1
+                            elif emotion_prediction == 'Surprised':
+                                self.captured_emotions[5] +=1
+                            elif emotion_prediction == 'Neutral':
+                                self.captured_emotions[6] +=1
                             cv2.putText(frame, "{}".format(emotion_prediction), (x2 - int((x2-x)/2) -30,y2+20), cv2.FONT_HERSHEY_SIMPLEX,0.7, self.labelColor,2)
                 except:
                     pass
+                max_ind = np.argmax(self.captured_emotions)
+                average_emotion = 0
+                if self.captured_emotions[max_ind] != 0:
+                    if max_ind == 0:
+                        self.average_emotions[0] +=1
+                        maxed_emotion = 'Angry'
+                    elif max_ind == 1:
+                        self.average_emotions[1] +=1
+                        maxed_emotion = 'Disgust'
+                    elif max_ind == 2:
+                        self.average_emotions[2] +=1
+                        maxed_emotion = 'Fear'
+                    elif max_ind == 3:
+                        self.average_emotions[3] +=1
+                        maxed_emotion = 'Happy'
+                    elif max_ind == 4:
+                        self.average_emotions[4] +=1
+                        maxed_emotion = 'Sad'
+                    elif max_ind == 5:
+                        self.average_emotions[5] +=1
+                        maxed_emotion = 'Surprised'
+                    elif max_ind == 6:
+                        self.average_emotions[6] +=1
+                        maxed_emotion = 'Neutral'
+                    average_emotion = self.captured_emotions[max_ind]/ np.sum(self.captured_emotions)
+                    #print("av: ", self.average_emotions)
+                #print("cap: ", maxed_emotion, " with ac: ", average_emotion)
                 if self.chosen == 1:
                     self.frame_sender(frame)
-                    print("Thread ", self.name, " sending")
-                else:
-                    print("Thread ", self.name, " not sending")
+                    #print("Thread ", self.name, " sending")
+                #else:
+                    #print("Thread ", self.name, " not sending")
             else:
                 if self.changePixmap:
                     self.ValChanged.emit(1)
@@ -280,10 +333,15 @@ class VideoThread(QThread):
     def stop(self):
         self.ThreadActive = False
         print("Stopped thread ", self.name)
+        #analys = list(self.captured_emotions)
+        analys = list(self.average_emotions/ np.sum(self.average_emotions))
+        self.Analysis_Thread.emit(analys)
         self.quit()
         
-class VideoMultiThread():  
+class VideoMultiThread(QThread):
+    Analysis = pyqtSignal(list)
     def __init__(self, cameraInds):
+        super().__init__()
         self.camerInds = cameraInds
         self.threadCount = len(cameraInds)
         self.currentThread = 0
@@ -296,6 +354,7 @@ class VideoMultiThread():
         for i in range(self.threadCount):
             self.frame = None
             self.threads[i].start()
+            self.threads[i].Analysis_Thread.connect(self.get_analysis)
         self.threads[self.currentThread].set_chosen(1)
         print("sending")
         
@@ -313,6 +372,9 @@ class VideoMultiThread():
     def stop_threads(self):
         for i in range(self.threadCount):
             self.threads[i].stop()
+    
+    def get_analysis(self, anal):
+        print("Analysis received from ", self.sender().name, " : ", anal)
     
     def getCurrentImageUpdate(self):
         print("THREADASDAS ", self.currentThread)
